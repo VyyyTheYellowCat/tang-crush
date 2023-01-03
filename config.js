244fe20620e4a76c8e5d8e8e9376cb493db7b488
VyyyTheYellowCat <99161542+VyyyTheYellowCat@users.noreply.github.com>
2023-01-03 20:22:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Bạn à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao cậu cứ đi nhầm đường
+    Lạc vào tim tớ lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnIntro: 'muhahaha',
+    title: 'Phải chăng tớ đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng tớ đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích cậu <33',
+    btnNo: 'Không, đâu bạn à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại thích tớ <3?',
+    btnReply: 'Gửi cho Dương <3',
+    reply: 'Vì cậu học giỏi và xinh đẹp <3',
+    mess: 'Tớ biết mà 🥰. Thích cậu nhiều nhiều 😘😘',
+    messDesc: 'Thế cậu có muốn ăn kem với tớ không?.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://images.dienmayhoanglong.vn/news/2015/3/18/kem-oc-que-ra-doi-khi-nao.1426648008.jpg'
 }
